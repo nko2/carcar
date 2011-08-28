4184537c8beb785b15a52e314bf75b5557d86943
Christiano Milfont <cmilfont@gmail.com>
2011-08-28 16:54:38 -0300

    mais testes

--- app.js
+++ app.js
@@ -21,19 +21,40 @@ app.get('/', function(req, res, next){
 app.use(express.errorHandler({ showStack: true }));
 app.use(express.static(__dirname));
 
-
 app.listen(80, function () {
   var addr = app.address();
   console.log('   app listening on http://' + addr.address + ':' + addr.port);
 });
 
+var stream;
 
 var io = sio.listen(app);
 io.set('log level', 1);
+
 io.sockets.on('connection', function (client) {
     
   client.on('user message', function (msg) {
     client.broadcast.emit('podcast', "MIlfont", msg);
+    if(stream) {
+        try {
+            stream.write(msg);
+        } catch(e) {
+            console.log(stream, e);
+        }
+    }
+    
   });
+  
+  client.on('start', function(name) {
+    stream = fs.createWriteStream("podcasts/" + name + ".wav");
+    stream.once('open', function(fd) {
+        
+    });
+  });
+  
+  client.on('stop', function() {
+    console.log("stop");
+    stream.end();
+  });  
 
 });
